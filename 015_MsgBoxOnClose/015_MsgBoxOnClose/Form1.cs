4210afdc7f9712a8ebf8e8df281b92e51b21f075
SV-97 <volzstefan97@googlemail.com>
2018-06-10 20:19:38 +0200

    Initial Commit

---
+++ 015_MsgBoxOnClose/015_MsgBoxOnClose/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _015_MsgBoxOnClose
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Bims zu");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Ich schließe jezt");
+        }
+    }
+}
---
+++ 015_MsgBoxOnClose/015_MsgBoxOnClose/Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _015_MsgBoxOnClose
+{
+    public partial class Master : Form
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void Master_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            Form1 f = new Form1();
+            f.ShowDialog();
+            MessageBox.Show("Dein Form ist gerade zu gegangen");
+            this.Close();
+        }
+    }
+}
